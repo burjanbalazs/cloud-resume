c9945f8cb8aac80b4fe37d03bbf03e4468f998a6
burjanb1@gmail.com <burjanb1@gmail.com>
2023-09-11 18:00:21 +0300

    updated the counter

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-    document.getElementById("visitorcounter").innerText = 'asd'
+    const counter = document.getElementById("visitorcounter")
+
+    const functionURL = "https://cloud-resume.azurewebsites.net/api/cloud-resume-visitorCounter?code=lWDMW1SBB8pNpWVEVVzKHIFfDuhPmAqFIoynky-QtI8-AzFuQiFQMQ=="
+
+    const requestData = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({name: 'Balazs'})
+    }
+
+    fetch(functionURL, requestData)
+    .then(response => {
+        if (!response.ok) {
+          throw new Error('HTTP error, status = ' + response.status);
+        }
+        return response.json(); // Parse the response body as JSON
+      })
+      .then(data => {
+        counter.innerHTML = data // Do something with the response data
+      })
+      .catch(error => {
+        console.error('Error:', error); // Handle any errors
+      });
 
 });
